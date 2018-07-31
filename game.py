e251393810aa76659125c48f60154d147ed36509
awesomelemonade <awesomelemonade@gmail.com>
2018-07-31 15:26:37 -0400

    Initial game.py

---
+++ game.py
@@ -0,0 +1,38 @@
+class Color:
+	BLACK = (0, 0, 0)
+	WHITE = (255, 255, 255)
+
+
+class TetrisGame:
+	def __init__(self):
+		self.close_requested = False
+		self.target_fps = 60
+		self.window_size = (800, 600)
+		self.title = "Tetris"
+	def start(self):
+		# Init
+		self.screen = pygame.display.set_mode(self.window_size)
+		pygame.display.set_caption(self.title)
+		self.clock = pygame.time.Clock()
+		# Main Game Loop
+		while not self.close_requested:
+			# Handles "X" button of window
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.close_requested = True
+			# Draws the game
+			self.screen.fill(Color.WHITE)
+			
+			#pygame.draw.rect(self.screen, Color.BLACK, [x, y, width, height], 0)
+			
+			pygame.display.flip()
+			self.clock.tick(self.target_fps)
+			
+
+import pygame
+pygame.init()
+game = TetrisGame()
+game.start()
+pygame.quit()
+
+
