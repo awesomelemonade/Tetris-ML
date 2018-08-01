4909e3ff6dc27bf860238865a5ab74ff683d57cb
awesomelemonade <awesomelemonade@gmail.com>
2018-08-01 10:01:11 -0400

    Added base mechanic of falling pieces

--- TetrisBoard.py
+++ TetrisBoard.py
@@ -1,21 +1,23 @@
 import numpy as np
+from TetrisConstants import *
+import pygame
 
 class TetrisBoard:
-	BLANK = '.'
 	def __init__(self, width, height, renderWidth, renderHeight, seed):
-		self.random = # TODO: Initialize Random with Seed
+		self.random = np.random.RandomState(seed) # Initialize random with seed
 		self.width = width
 		self.height = height
-		self.grid = createBlankGrid()
+		self.grid = self.createBlankGrid()
 		self.startX = int(width / 2 - TetrisConstants.TEMPLATE_WIDTH / 2)
 		self.startY = 0
-		self.fallingPiece = Piece(startX, startY, getRandomPieceType(), )
+		self.fallingPiece = self.getRandomNewPiece()
+		self.nextPiece = self.getRandomNewPiece()
 	def createBlankGrid(self):
 		grid = np.chararray((self.width, self.height))
 		grid[:] = BLANK
 		return grid
 	def isCompleteLine(self, grid, y):
-		return np.all(board[:, y] != BLANK)
+		return np.all(grid[:, y] != BLANK)
 	def removeCompletedLines(self):
 		numLinesRemoved = 0
 		for y in range(self.height, 0, -1) - 1:
@@ -23,35 +25,54 @@ class TetrisBoard:
 				numLinesRemoved += 1
 		self.grid[:, numLinesRemoved:] = self.grid[:, :height - numLinesremoved]
 		self.grid[:, :numLinesRemoved] = BLANK
-	def isOnBoard(x, y):
+	def isOnBoard(self, x, y):
 		return x >= 0 and x < self.width and y >= 0 and y < self.height
-	def isValidPosition(piece, adjustX=0, adjustY=0):
+	def isValidPosition(self, piece, adjustX=0, adjustY=0):
 		for x in range(TetrisConstants.TEMPLATE_WIDTH):
 			for y in range(TetrisConstants.TEMPLATE_HEIGHT):
-				if getTemplate(piece) == BLANK
+				if self.getTemplate(piece, x, y) == BLANK:
 					continue
-				if not isOnBoard(piece.x + x + adjustX, piece.y + y + adjustY):
+				if not self.isOnBoard(piece.x + x + adjustX, piece.y + y + adjustY):
 					return False
-				if self.grid[piece.x + x + adjustX, piece.y + y + adjustY] != BLANK:
+				if self.grid[piece.x + x + adjustX, piece.y + y + adjustY].decode() != BLANK:
 					return False
 		return True
 	def setPiece(self, piece):
 		for x in range(TetrisConstants.TEMPLATE_WIDTH):
 			for y in range(TetrisConstants.TEMPLATE_HEIGHT):
-				if not TetrisConstants.PIECES[piece.type] == BLANK:
-					grid[piece.x + x, piece.y + y] = getTemplate(piece)[x, y]
-	def getTemplate(self, piece):
-		return TetrisConstants.PIECES[piece.type].template[piece.rotation]
+				if not self.getTemplate(piece, x, y) == BLANK:
+					self.grid[piece.x + x, piece.y + y] = self.getTemplate(piece, x, y)
+	def getTemplate(self, piece, x=None, y=None):
+		if x is None or y is None:
+			return TetrisConstants.PIECES[piece.type].template[piece.rotation]
+		else:
+			return TetrisConstants.PIECES[piece.type].template[piece.rotation][x][y]
 	def getRandomNewPiece(self):
-		pieceType = getRandomPieceType()
-		return Piece(self.startX, self.startY, pieceType, getRandomRotation(pieceType))
+		pieceType = self.getRandomPieceType()
+		return Piece(self.startX, self.startY, pieceType, self.getRandomRotation(pieceType))
 	def getRandomPieceType(self):
-		return random.randint(len(PIECES.keys()))
+		return self.random.choice(list(TetrisConstants.PIECES.keys()))
 	def getRandomRotation(self, pieceType):
-		return random.randint(len(PIECES[pieceType].template))
+		return self.random.randint(0, len(TetrisConstants.PIECES[pieceType].template))
 	def update(self):
-		
+		if not self.isValidPosition(self.fallingPiece, adjustY=1):
+			# falling piece has landed, set it on the board
+			self.setPiece(self.fallingPiece)
+			self.fallingPiece = self.nextPiece
+			self.nextPiece = self.getRandomNewPiece()
+		else:
+			self.fallingPiece.y += 1
 	def render(self, screen, renderWidth, renderHeight):
+		gridWidth = renderWidth / self.width
+		gridHeight = renderHeight / self.height
+		# Render Grid
 		for x in range(self.width):
 			for y in range(self.height):
-				pygame.draw.rect(screen, TetrisConstants.PIECES[self.grid[x, y]].color, [x, y, renderWidth/width, renderHeight/height])
+				if not self.grid[x, y].decode() == BLANK:
+					pygame.draw.rect(screen, TetrisConstants.PIECES[self.grid[x, y].decode()].color, [x * gridWidth, y * gridHeight, gridWidth, gridHeight])
+		# Render Falling Piece
+		for x in range(TetrisConstants.TEMPLATE_WIDTH):
+			for y in range(TetrisConstants.TEMPLATE_HEIGHT):
+				if not self.getTemplate(self.fallingPiece, x, y) == BLANK:
+					pygame.draw.rect(screen, TetrisConstants.PIECES[self.fallingPiece.type].color, [(self.fallingPiece.x + x) * gridWidth, (self.fallingPiece.y + y) * gridHeight, gridWidth, gridHeight])
+		# Render Next Piece - TODO
--- game.py
+++ game.py
@@ -1,17 +1,18 @@
-import Color
-import TetrisConstants
+from Color import Color
+from TetrisBoard import TetrisBoard
 
 class TetrisGame:
 	def __init__(self):
 		self.close_requested = False
-		self.target_fps = 60
-		self.window_size = (800, 600)
+		self.target_fps = 3
+		self.window_size = (10 * 40, 20 * 40)
 		self.title = "Tetris"
 	def start(self):
 		# Init
 		self.screen = pygame.display.set_mode(self.window_size)
 		pygame.display.set_caption(self.title)
 		self.clock = pygame.time.Clock()
+		board = TetrisBoard(10, 20, *self.window_size, seed=0)
 		# Main Game Loop
 		while not self.close_requested:
 			# Handles "X" button of window
@@ -19,14 +20,15 @@ class TetrisGame:
 				if event.type == pygame.QUIT:
 					self.close_requested = True
 			# Draws the game
-			self.screen.fill(Color.WHITE)
+			self.screen.fill(Color.BLACK)
 			
+			board.update()
+			board.render(self.screen, *self.window_size)
 			#pygame.draw.rect(self.screen, Color.BLACK, [x, y, width, height], 0)
 			
 			pygame.display.flip()
 			self.clock.tick(self.target_fps)
 			
-
 
 
 import pygame
