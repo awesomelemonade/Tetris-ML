b505c97585cd29d35e8c37045b937f2026bf0262
awesomelemonade <awesomelemonade@gmail.com>
2018-08-03 10:24:09 -0400

    Finished initial theoretical TetrisML

--- TetrisML.py
+++ TetrisML.py
@@ -12,25 +12,82 @@ class AbstractModel:
 			self.weights.append(mg.Tensor(np.random.randn(layers[i], layers[i + 1])))
 	def policyForward(self, data):
 		data = mg.Tensor(data)
-		for i in range(len(weights) - 1):
+		for i in range(len(self.weights) - 1):
 			data = mg.matmul(data, self.weights[i]) # hidden layers
 			data[data < 0] = 0 # ReLU
 		return mg.matmul(data, self.weights[-1]) # outNeurons
 	def getDerivatives(self):
-		return (weight.grad for weight in self.weights)
-	def gradientDescent(self, derivatives, rewards):
-		rewards = np.array(rewards, dtype=np.float64)
-		zipped = np.moveaxis(np.array(weight), 0, -1) for weight in zip(*derivatives)
-		for weight, derivative in zip(self.weights, derivatives):
-			weight += np.sum(self.learning_rate * derivative * rewards, axis=-1)
+		return [weight.grad for weight in self.weights]
+	def gradientDescent(self, learning_rate, derivatives, rewards):
+		# reward dimension = board id, turn #
+		# derivative dimension = board id, turn #, weight #
+		for boardDerivatives, boardRewards in zip(derivatives, rewards): # Loops through board id
+			# boardDerivatives dimension = turn #, weight #, nparrays
+			zipped = [np.moveaxis(np.array(weight), 0, -1) for weight in zip(*boardDerivatives)]
+			# zipped dimension = weight #, turn #, nparrays
+			boardRewards = np.array(boardRewards, dtype=np.float64)
+			for weight, derivative, reward in zip(self.weights, zipped, boardRewards):
+				weight -= np.sum(learning_rate * derivative * reward, axis=-1)
 class TetrisModel:
-	def __init__(self):
-		self.H = 200 # Number of hidden neurons
-		self.batch_size = 10 # Play multiple games simultaneously
+	def __init__(self, size):
+		self.size = size
+		self.layers = [200, 50, 50, 5]
+		self.model = AbstractModel(self.layers)
 		self.learning_rate = 1e-4
-		self.model = {}
-		self.model['W1'] = mg.Tensor(np.random.randn(200, self.H))
-		self.model['W2'] = mg.Tensor(np.random.randn(self.H, 5))
+		self.reset()
+		self.cache = {}
+		self.cache['arange'] = np.arange(self.layers[-1])
+		self.cache['choice'] = []
+		for i in range(self.layers[-1]):
+			temp = np.zeros(self.layers[-1], dtype=int)
+			temp[i] = 1
+			self.cache['choice'].append(temp)
+	def reset(self):
+		self.derivatives = []
+		self.rewards = []
+		for i in range(self.size):
+			self.derivatives.append([])
+			self.rewards.append([])
+	def preprocess(self, tetrisBoard): # Preprocesses the tetris board to a flattened numpy array
+		# Process Board
+		grid = tetrisBoard.grid != BLANK # Grid of 0's and 1's
+		grid = grid.astype(int) # Convert Boolean to Integer
+		# Process Falling Piece
+		for x in range(TetrisConstants.TEMPLATE_WIDTH):
+			for y in range(TetrisConstants.TEMPLATE_HEIGHT):
+				if not tetrisBoard.getTemplate(tetrisBoard.fallingPiece, x, y) == BLANK:
+					grid[tetrisBoard.fallingPiece.x + x, tetrisBoard.fallingPiece.y + y] = -1
+		# Process Next Piece TODO
+		return grid.ravel() # Flatten
+	def step(self, tetrisBoards, executor): # calculates and stores derivatives
+		for i, tetrisBoard in enumerate(tetrisBoards):
+			#temp = time.time()
+			data = self.preprocess(tetrisBoard)
+			#print("Preprocess: {}".format(time.time() - temp))
+			#temp = time.time()
+			outNeurons = self.model.policyForward(data)
+			probabilities = softmax(outNeurons)
+			#print("Forward: {}".format(time.time() - temp))
+			#temp = time.time()
+			choice = np.random.choice(self.cache['arange'], p=probabilities)
+			reward = self.cache['choice'][choice]
+			#print("Choice: {}".format(time.time() - temp))
+			#temp = time.time()
+			loss = softmax_crossentropy(outNeurons.reshape(1, len(outNeurons)), reward)
+			loss.backward()
+			#print("Backprop: {}".format(time.time() - temp))
+			#temp = time.time()
+			self.derivatives[i].append(self.model.getDerivatives())
+			loss.null_gradients()
+			#print("Store: {}".format(time.time() - temp))
+			executor(tetrisBoard, choice)
+	def evaluate(self, tetrisBoards): # calculates rewards
+		for i, tetrisBoard in enumerate(tetrisBoards):
+			for j in range(len(self.derivatives[i])):
+				self.rewards[i].append(0)
+	def gradientDescent(self):
+		self.model.gradientDescent(self.learning_rate, self.derivatives, self.rewards)
+	'''
 	def policyForward(self, data):
 		data = mg.Tensor(data.astype(int)) # Convert to Tensor
 		hiddenNeurons = mg.matmul(data, self.model['W1']) # Matrix multiply weights #1
@@ -39,63 +96,15 @@ class TetrisModel:
 		return outNeurons
 	def getDerivatives(self): # Extracts derivatives from backprop
 		return (self.model['W1'].grad, self.model['W2'].grad)
-	def gradientDescent(self, derivatives, rewards): # rewards are delta score
-		rewards = np.array(rewards, dtype=np.float64)
+	def gradientDescent(self): # rewards are delta score
+		rewards = np.array(self.rewards, dtype=np.float64)
 		# derivatives = list of list
-		dW1, dW2 = zip(*derivatives)
+		dW1, dW2 = zip(*self.derivatives)
 		dW1 = np.moveaxis(np.array(dW1), 0, -1)
 		dW2 = np.moveaxis(np.array(dW2), 0, -1)
 		rewards[-1] = - 1 / len(rewards) # discourage all steps slightly because we lost
 		rewards = np.cumsum(rewards[::-1])[::-1]
 		self.model['W1'] += np.sum(self.learning_rate * dW1 * rewards, axis=-1)
 		self.model['W2'] += np.sum(self.learning_rate * dW2 * rewards, axis=-1)
-class TetrisML:
-	def __init__(self):
-		self.model = TetrisModel()
-		self.derivatives = []
-		self.rewards = []
-		self.prevscores = {}
-		self.cache = {}
-		self.cache['arange'] = np.arange(5)
-		self.cache['choice'] = []
-		for i in range(5):
-			temp = np.zeros(5, dtype=int)
-			temp[i] = 1
-			self.cache['choice'].append(temp)
-	def preprocess(self, tetrisBoard): # Preprocesses the tetris board to a flattened numpy array
-		grid = tetrisBoard.grid != BLANK
-		for x in range(TetrisConstants.TEMPLATE_WIDTH):
-			for y in range(TetrisConstants.TEMPLATE_HEIGHT):
-				if not tetrisBoard.getTemplate(tetrisBoard.fallingPiece, x, y) == BLANK:
-					grid[tetrisBoard.fallingPiece.x + x, tetrisBoard.fallingPiece.y + y] = 2
-		return grid.ravel()
-	def step(self, tetrisBoard): # Called each step of the game
-		temp = time.time()
-		data = self.preprocess(tetrisBoard)
-		print("Preprocess: {}".format(time.time() - temp))
-		temp = time.time()
-		outNeurons = self.model.policyForward(data)
-		probabilities = softmax(outNeurons)
-		print("Forward: {}".format(time.time() - temp))
-		temp = time.time()
-		choice = np.random.choice(self.cache['arange'], p=probabilities)
-		reward = self.cache['choice'][choice]
-		print("Choice: {}".format(time.time() - temp))
-		temp = time.time()
-		loss = softmax_crossentropy(outNeurons.reshape(1, len(outNeurons)), reward)
-		loss.backward()
-		print("Backprop: {}".format(time.time() - temp))
-		temp = time.time()
-		# Store the derivatives - model['W1'] and model['W2']
-		self.derivatives.append(self.model.getDerivatives())
-		loss.null_gradients()
-		# Calculate delta score, store
-		deltaScore = tetrisBoard.score - self.prevscores.get(tetrisBoard, 0)
-		self.prevscores[tetrisBoard] = tetrisBoard.score
-		self.rewards.append(deltaScore)
-		print("Record: {}".format(time.time() - temp))
-		return choice
-	def gameover(self): # Called when lose to gradient descent
-		self.model.gradientDescent(self.derivatives, self.rewards)
-		self.derivatives = []
-		self.rewards = []
+		self.reset()
+	'''
--- TetrisTrainer.py
+++ TetrisTrainer.py
@@ -1,35 +1,36 @@
 import numpy as np
 from TetrisBoard import TetrisBoard
-from TetrisML import TetrisML
+from TetrisML import *
 from Color import Color
 import time
 class TetrisTrainer:
 	def start(self):
-		env = TetrisML()
+		numBoards = 20
+		env = TetrisModel(numBoards)
 		self.BLOCK_SIZE = 40
 		self.window_size = (3 * 10 * self.BLOCK_SIZE, 1 * 20 * self.BLOCK_SIZE)
-		self.screen = pygame.display.set_mode(self.window_size)
-		pygame.display.set_caption("Tetris Trainer")
+		#self.screen = pygame.display.set_mode(self.window_size)
+		#pygame.display.set_caption("Tetris Trainer")
 		self.clock = pygame.time.Clock()
 		for i in range(100):
 			#print(np.sum(env.model.model['W1'].data))
-			board = TetrisBoard(10, 20, seed=0)
-			while True:
+			boards = [TetrisBoard(10, 20, seed=0) for j in range(numBoards)]
+			done = np.zeros(len(boards))
+			while not np.all(done):
 				temp = time.time()
-				action = env.step(board)
+				generator = (boards[j] for j in range(len(done)) if not done[j])
+				env.step(boards, self.execute)
+				for j, board in enumerate(boards):
+					if not board.update():
+						done[j] = True
+				env.evaluate(boards)
+			print("Descending: {}".format(i))
+			env.gradientDescent()
+			'''
 				print("Stepping Environment: {}".format(time.time() - temp))
 				temp = time.time()
-				if action == 0:
-					board.rotateFallingPiece(1)
-				if action == 1:
-					board.moveFallingPiece(moveX=-1, moveY=0)
-				if action == 2:
-					board.moveFallingPiece(moveX=1, moveY=0)
-				if action == 3:
-					board.moveFallingPiece(moveX=0, moveY=1)
 				print("Executing Move: {}".format(time.time() - temp))
 				temp = time.time()
-				# Action #4 does nothing
 				if not board.update():
 					env.gameover()
 					break
@@ -40,8 +41,18 @@ class TetrisTrainer:
 				pygame.display.flip()
 				print("Rendering Board: {}".format(time.time() - temp))
 				self.clock.tick(60)
-			print("Score: {}".format(board.score))
-
+			print("Score: {}".format(board.score))'''
+	def execute(self, board, action):
+		if action == 0:
+			board.rotateFallingPiece(1)
+		if action == 1:
+			board.moveFallingPiece(moveX=-1, moveY=0)
+		if action == 2:
+			board.moveFallingPiece(moveX=1, moveY=0)
+		if action == 3:
+			board.moveFallingPiece(moveX=0, moveY=1)
+		if action == 4:
+			pass # do nothing
 
 import pygame
 pygame.init()
