caf53e35a6243b037e120382d7737a850d493764
awesomelemonade <awesomelemonade@gmail.com>
2018-08-04 22:29:43 -0400

    Added installDependencies.sh

---
+++ installDependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Installing Dependencies..."
+pip3 install torch torchvision
+pip3 install recordclass
+pip3 install numpy
+pip3 install numba
+pip3 install scipy
+pip3 install pygame
